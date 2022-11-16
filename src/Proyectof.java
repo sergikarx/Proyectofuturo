1eef943b914a62b9055f0039b55d491ae5fea8ba
SergioArocaMartínez <sergikarx@gmail.com>
2022-11-16 23:54:17 +0100

    Primer commit

---
+++ src/Proyectof.java
@@ -0,0 +1,19 @@
+public class Proyectof {
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + " : ");
+        System.out.println(veredicto("6","0"));
+
+        //
+    }
+
+    static public String veredicto (String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+
+}
